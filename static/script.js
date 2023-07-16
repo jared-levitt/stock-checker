f60145caced3b7cb2380c2f1bfb134dd69c63e1a
jared-levitt <54280491+jared-levitt@users.noreply.github.com>
2023-07-16 19:48:30 -0400

    Update script.js

--- static/script.js
+++ static/script.js
@@ -1,8 +1,11 @@
-// script.js
-function updateLastRefresh() {
-  var now = new Date();
-  var lastRefreshElement = document.getElementById('last-refresh');
-  lastRefreshElement.textContent = 'Last Refresh: ' + now.toLocaleString();
-}
+// Get the current date and time
+var currentDate = new Date();
 
-updateLastRefresh();
+// Format the date as yyyy/mm/dd
+var formattedDate = currentDate.getFullYear() + '/' + (currentDate.getMonth() + 1) + '/' + currentDate.getDate();
+
+// Format the time as military time in hours and minutes
+var formattedTime = currentDate.getHours().toString().padStart(2, '0') + ':' + currentDate.getMinutes().toString().padStart(2, '0');
+
+// Display the formatted date and time
+document.getElementById('last-updated').textContent = formattedDate + ' ' + formattedTime;
