790cadb8e9399aa73542096224008690fe803040
jared-levitt <54280491+jared-levitt@users.noreply.github.com>
2023-07-16 10:42:49 -0400

    Delete Stock Checker/old directory

--- Stock Checker/old/static/script.js
+++
@@ -1,8 +0,0 @@
-// script.js
-document.addEventListener('DOMContentLoaded', function() {
-    var button = document.getElementById('submitButton');
-    button.addEventListener('click', function() {
-        var ticker = document.getElementById('ticker').value;
-        window.location.href = '/stock-price/' + ticker;
-    });
-});
