11c3a20e903b5bda0b60dc2c3660ea1934beb1ba
jared-levitt <54280491+jared-levitt@users.noreply.github.com>
2023-07-17 16:22:29 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,48 +1,49 @@
 import os
-import requests
-import finnhub
-from flask import Flask, render_template, request, redirect, url_for
-from pyfiglet import Figlet
+import time
 import re
-from flask_socketio import SocketIO, emit
-from apscheduler.schedulers.background import BackgroundScheduler
-import datetime
+from flask import Flask, render_template, request, redirect, url_for
+from flask_socketio import SocketIO
+from datetime import datetime
+
+import finnhub
+finnhub_client = finnhub.Client(api_key="ciqmtm9r01qjff7crg10ciqmtm9r01qjff7crg1g")
 
 app = Flask(__name__)
-app.secret_key = os.urandom(24)
+app.config['SECRET_KEY'] = os.urandom(24)
+
 socketio = SocketIO(app)
 
 def get_stock_price(symbol):
-    api_key = 'ciqpdm9r01qjff7css00ciqpdm9r01qjff7css0g'
-    finnhub_client = finnhub.Client(api_key=api_key)
-    quote = finnhub_client.quote(symbol)
-    stock_price = quote['c']
-    change = round(float(quote['d']), 2)
-    changePercent = round(float(quote['dp']), 2)
-    high = round(float(quote['h']), 2)
-    low = round(float(quote['l']), 2)
-    return stock_price, change, changePercent, high, low
+    try:
+        data = finnhub_client.quote(symbol)
+        print(data)
+        stock_price = data['c']
+        change = round(float(data['d']), 2)
+        changePercent = round(float(data['dp']), 2)
+        high = round(float(data['h']), 2)
+        low = round(float(data['l']), 2)
+        return stock_price, change, changePercent, high, low
+    except Exception as e:
+        print(f"Error retrieving stock price: {e}")
+        return None, None, None, None, None
 
 def emit_stock_update():
-    stock_symbol = request.args.get('symbol')
-    price, change, changePercent, high, low = get_stock_price(stock_symbol)
+    stock_price, change, changePercent, high, low = get_stock_price(stock_symbol)
+    last_updated = time.strftime("%d/%m/%Y %H:%M")
 
-    if price:
-        formatted_price = re.sub('[^0-9.]', '', price)
-        formatted_price = formatted_price[:-2]
-        ascii_price = Figlet(font='colossal').renderText(' '.join(formatted_price))
-
-        emit('stock_update', {
-            'price': ascii_price,
-            'change': change,
-            'changePercent': changePercent,
-            'high': high,
-            'low': low
-        }, broadcast=True)
-        emit('time_update', {'lastUpdated': datetime.datetime.now().strftime('%d/%m/%Y %H:%M')}, broadcast=True)
+    socketio.emit('stock_update', {
+        'price': stock_price,
+        'change': change,
+        'changePercent': changePercent,
+        'high': high,
+        'low': low,
+        'lastUpdated': last_updated
+    }, broadcast=True)
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
+    global stock_symbol
+
     if request.method == 'POST':
         stock_symbol = request.form['symbol']
         return redirect(url_for('stock_price', symbol=stock_symbol))
@@ -51,20 +52,19 @@ def index():
 
 @app.route('/stock-price/<symbol>')
 def stock_price(symbol):
-    price, change, changePercent, high, low = get_stock_price(symbol)
+    current_time = datetime.now().strftime("%d/%m/%Y %H:%M")
+    return render_template('stock_price.html', stock_symbol=symbol, current_time=current_time)
 
-    if price:
-        formatted_price = re.sub('[^0-9.]', '', str(price))
-        spaced_price = ' '.join(formatted_price)
-        
-        font = Figlet(font='colossal')
-        ascii_price = font.renderText(spaced_price)
+@socketio.on('connect')
+def handle_connect():
+    print('Connected to server')
 
-        return render_template('stock_price.html', ascii_price=ascii_price, stock_symbol=symbol, change=change, changePercent=changePercent, high=high, low=low)
-    else:
-        return 'Error retrieving stock price'
+@socketio.on('disconnect')
+def handle_disconnect():
+    print('Disconnected from server')
 
 if __name__ == '__main__':
+    from apscheduler.schedulers.background import BackgroundScheduler
     scheduler = BackgroundScheduler()
     scheduler.add_job(emit_stock_update, 'interval', seconds=5)
     scheduler.start()
