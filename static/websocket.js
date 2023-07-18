69269470780b081964ab5dd8aae80c2dd5f902da
jared-levitt <54280491+jared-levitt@users.noreply.github.com>
2023-07-18 11:29:40 -0400

    Update websocket.js

--- static/websocket.js
+++ static/websocket.js
@@ -17,7 +17,7 @@ function updateStockData(data) {
     const { price, change, changePercent, high, low, last_updated, ascii_price } = data;
 
     // Update stock price and change
-    const asciiPriceElement = document.querySelector('#ascii-price');
+    const asciiPriceElement = document.querySelector('.rainbow-text');
     const changeElement = document.querySelector('.change');
     const changePercentElement = document.querySelector('.changePercent');
     const highElement = document.querySelector('.high');
@@ -29,6 +29,6 @@ function updateStockData(data) {
     changePercentElement.innerHTML = `Change Percent: <span style="color: ${changePercent > 0 ? '#90fd58' : '#ff6767'};">${changePercent}%</span>`;
     highElement.innerHTML = `High: <span style="color: #90fd58; font-size: 24px;">&#11014;&#xFE0E</span> ${high}`;
     lowElement.innerHTML = `Low: <span style="color: #ff6767; font-size: 24px;">&#11015;&#xFE0E</span> ${low}`;
-    lastUpdatedElement.textContent = `Last Updated: ${lastUpdated}`;
-    asciiPriceElement.textContent = asciiPrice;
+    lastUpdatedElement.textContent = `${last_updated}`;
+    asciiPriceElement.textContent = ascii_price;
 }
