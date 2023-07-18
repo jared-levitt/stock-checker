3d42cc2d58ed74c83c5c5383801619dc5904c926
jared-levitt <54280491+jared-levitt@users.noreply.github.com>
2023-07-18 09:46:16 -0400

    Update app.py to cache and check prices

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ from flask import Flask, render_template, request, redirect, url_for
 from flask_socketio import SocketIO
 from datetime import datetime
 from pyfiglet import Figlet
+
 import finnhub
 
 finnhub_client = finnhub.Client(api_key="ciqqfbhr01qjff7ctobgciqqfbhr01qjff7ctoc0")
@@ -14,46 +15,53 @@ app.config['SECRET_KEY'] = os.urandom(24)
 
 socketio = SocketIO(app)
 
-stock_symbol = None
+cache = {}
+update_interval = 15  # Update interval in seconds
 
 def get_stock_price(symbol):
+    # Check if data exists in the cache and if it is still valid
+    if symbol in cache and time.time() - cache[symbol]['last_updated'] <= update_interval:
+        return cache[symbol]['data']
+
+    # Fetch new data from the API
     try:
         data = finnhub_client.quote(symbol)
-        print(data)
         stock_price = data['c']
         change = round(float(data['d']), 2)
         changePercent = round(float(data['dp']), 2)
         high = round(float(data['h']), 2)
         low = round(float(data['l']), 2)
 
-        if stock_price:
-            formatted_price = re.sub('[^0-9.]', '', str(stock_price))
-            spaced_price = ' '.join(formatted_price)
+        # Update cache with new data and last update time
+        cache[symbol] = {
+            'data': (stock_price, change, changePercent, high, low),
+            'last_updated': time.time()
+        }
 
-            font = Figlet(font='colossal')
-            ascii_price = font.renderText(spaced_price)
-
-            return stock_price, change, changePercent, high, low, ascii_price
-        else:
-            return None, None, None, None, None, None
-
+        return cache[symbol]['data']
     except Exception as e:
         print(f"Error retrieving stock price: {e}")
-        return None, None, None, None, None, None
+        return None, None, None, None, None
 
 def emit_stock_update():
-    stock_price, change, changePercent, high, low, ascii_price = get_stock_price(stock_symbol)
+    stock_price, change, changePercent, high, low = get_stock_price(stock_symbol)
     last_updated = time.strftime("%d/%m/%Y %H:%M")
 
-    socketio.emit('stock_update', {
-        'price': stock_price,
-        'change': change,
-        'changePercent': changePercent,
-        'high': high,
-        'low': low,
-        'lastUpdated': last_updated,
-        'asciiPrice': ascii_price
-    }, broadcast=True)
+    if stock_price is not None:
+        formatted_price = re.sub('[^0-9.]', '', str(stock_price))
+        spaced_price = ' '.join(formatted_price)
+
+        font = Figlet(font='colossal')
+        ascii_price = font.renderText(spaced_price)
+
+        socketio.emit('stock_update', {
+            'price': ascii_price,
+            'change': change,
+            'changePercent': changePercent,
+            'high': high,
+            'low': low,
+            'lastUpdated': last_updated
+        }, broadcast=True)
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
@@ -68,12 +76,8 @@ def index():
 @app.route('/stock-price/<symbol>')
 def stock_price(symbol):
     current_time = datetime.now().strftime("%d/%m/%Y %H:%M")
-    stock_price, change, changePercent, high, low, ascii_price = get_stock_price(symbol)
-
-    if stock_price:
-        return render_template('stock_price.html', ascii_price=ascii_price, stock_symbol=symbol, change=change, changePercent=changePercent, high=high, low=low, current_time=current_time)
-    else:
-        return 'Error retrieving stock price'
+    stock_price, change, changePercent, high, low = get_stock_price(symbol)
+    return render_template('stock_price.html', stock_symbol=symbol, current_time=current_time, stock_price=stock_price, change=change, changePercent=changePercent, high=high, low=low)
 
 @socketio.on('connect')
 def handle_connect():
